642d24071aeb0f1e0bf88c96c4abe87fafb0fabb
sachinsarmy <106675309+sachinsarmy@users.noreply.github.com>
2026-02-28 14:57:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ async def approve_and_send(update: Update, context: ContextTypes.DEFAULT_TYPE):
 ♻सहायता के लिए @SYVOX007
 
 🔴हैक का उपयोग कैसे करें
-https://t.me/rajaindiaprediction/54
+https://t.me/+Lbu7qtsKpSgwYTUx
 """
             )
 
@@ -72,7 +72,7 @@ https://t.me/rajaindiaprediction/54
                 voice=voice,
                 caption="""
 🎙 सदस्य 9X गुना लाभ का प्रमाण 👇🏻
-https://t.me/rajaindiaprediction/56
+https://t.me/+Lbu7qtsKpSgwYTUx
 
 ♻सहायता के लिए @SYVOX007
 लगातार नंबर पे नंबर जीतना 🤑♻👑
